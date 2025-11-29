3f1816b2ec7469e4672468410d0d7de3515ba996
Barkin Yarici <barkinyarici@Barkins-MacBook-Air.local>
2025-11-29 13:44:58 -0600

    Initial commit

---
+++ delete.py
@@ -0,0 +1,2 @@
+from langchain import hub
+print(hub.list())
--- main.py
+++ main.py
@@ -1,39 +1,33 @@
-from asyncio import timeout
-
 from dotenv import load_dotenv
 load_dotenv()
 
 from langchain.agents import create_agent
-from langchain.tools import tool
-from langchain_core.messages import HumanMessage
+from langgraph.checkpoint.memory import MemorySaver
+
 from langchain_openai import ChatOpenAI
-from tavily import TavilyClient
+from langchain_tavily import TavilySearch
 
-tavily = TavilyClient()
+tools = [TavilySearch()]
+llm = ChatOpenAI(model="gpt-4")
 
+# modern memory API
+memory = MemorySaver()
 
-
-@tool
-def search(query: str) -> str:
-    """
-    Tool that searches over internet
-    Args:
-        query: The query to search for
-    Returns:
-        The search result
-    """
-    print(f"Searching for {query}")
-    return tavily.search(query=query, timeout=380)
-
-
-llm= ChatOpenAI(model= "gpt-5")
-tools = [search]
-agent = create_agent(model=llm, tools=tools)
-
+agent = create_agent(
+    model=llm,
+    tools=tools,
+    checkpointer=memory
+)
 
 def main():
-    print("Hello from langchain-course!")
-    result= agent.invoke({"messages": HumanMessage(content= "Search me 1 job postions using langchain, return titles only")})
+    result = agent.invoke(
+        {"messages": [
+            {
+                "role": "user",
+                "content": "Give me 3 AI engineer job openings"}]},
+        config={"configurable": {"thread_id": "session-1"}}
+    )
+
     print(result)
 
 if __name__ == "__main__":
